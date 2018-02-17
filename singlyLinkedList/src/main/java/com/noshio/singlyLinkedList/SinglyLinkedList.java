13ff65e416bea05cd791b54e66d052f1f9d0b775
Jose Martins <josetabordamartins@gmail.com>
2018-02-17 21:01:34 +0000

    Initial Commit

---
+++ singlyLinkedList/src/main/java/com/noshio/singlyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,341 @@
+package com.noshio.singlyLinkedList;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Implementation of a single linked list that can store any type of objects.
+ * this implementation allows iterating over the list and reverse it.
+ * @author martins
+ *
+ * @param <T>
+ */
+public class SinglyLinkedList<T> implements Iterable<T> {
+
+	@SuppressWarnings("rawtypes")
+	private Node head;
+	@SuppressWarnings("rawtypes")
+	private Node tail;
+
+	/**
+	 * Adds a new element to the list
+	 * 
+	 * @param element
+	 */
+	@SuppressWarnings("unchecked")
+	public void add(T element) {
+
+		Node<T> node = new Node<T>();
+		node.setData(element);
+		/**
+		 * is it the first one ?
+		 */
+		if (head == null) {
+			// there is only one element, so head and tail
+			// will point to the same object
+			head = node;
+			tail = node;
+		} else {
+			// we have previous nodes, so
+			// tail has to point to the next node
+			tail.setNext(node);
+			tail = node;
+		}
+	}
+
+	public boolean addIfAbsent(T element) {
+		if (head == null) { // the list is empty...so just add it and terminate
+			add(element);
+			return true;
+		}
+		// list is not empty...so we check if element exists
+		if (contains(element)) {
+			return false;
+		} else {
+			add(element);
+			return true;
+		}
+	}
+
+	/**
+	 * Adds a new element after the specified element
+	 * 
+	 * @param element to insert
+	 * @param after this element
+	 * @return void
+	 */
+	public void addAfter(T element, T after) {
+		if (head==null) {
+			throw new IndexOutOfBoundsException("the referenced SingleListedLink has no elements");
+		}
+		@SuppressWarnings("unchecked")
+		Node<T> tmp = head;
+		Node<T> refNode = null;
+
+		/**
+		 * we have to traverse all nodes untill the given element is found.
+		 */
+		while (true) {
+			if (tmp == null) {
+				break;
+			}
+			if (tmp.compareTo(after) == 0) {
+				// we found the node with given data. adding after
+				refNode = tmp;
+				break;
+			}
+			tmp = tmp.next();
+		}
+		if (refNode != null) {
+			// add the element after the target found above
+			Node<T> node = new Node<T>();
+			node.setData(element);
+			node.setNext(tmp.next());
+			if (tmp == tail) {
+				tail = node;
+			}
+			tmp.setNext(node);
+
+		} else {
+			throw new NoSuchElementException("element could not be found in the referenced SingleListedLink");
+		}
+	}
+
+	/**
+	 * deletes the head element of the list
+	 */
+	public void deleteFront() {
+		if (head == null) {
+			throw new IndexOutOfBoundsException("the referenced SingleListedLink has no elements");
+		}
+		@SuppressWarnings("unchecked")
+		Node<T> tmp = head;// swap head with next node
+		head = tmp.next();
+		if (head == null) { // in case the list gets empty.
+			tail = null;
+		}
+	}
+
+	/**
+	 * deletes the element immediately after the supplied element
+	 * @param after
+	 */
+	public void deleteAfter(T after) {
+		if (head==null) {
+			throw new IndexOutOfBoundsException("the referenced SingleListedLink has no elements");
+		}
+		@SuppressWarnings("unchecked")
+		Node<T> tmp = head;
+		Node<T> refNode = null;
+		/**
+		 * we have to traverse all nodes until the given element is found.
+		 */
+		while (true) {
+			if (tmp == null) { // list is empty
+				break;
+			}
+			if (tmp.compareTo(after) == 0) { // target Node found
+				refNode = tmp;
+				break;
+			}
+			tmp = tmp.next();
+		}
+		if (refNode != null) {
+			tmp = refNode.next();
+			refNode.setNext(tmp.next());
+			if (refNode.next() == null) {
+				tail = refNode;
+			}
+		} else {
+			throw new NoSuchElementException("element could not be found in the referenced SingleListedLink");
+		}
+	}
+	
+
+	
+	/**
+	 * reverses the order of the elements on the list
+	 * @return void
+	 */
+	public void  reverse() {
+		@SuppressWarnings("unchecked")
+		Node<T> currentNode=head;
+		Node<T> nextNode=null;
+		Node<T> previousNode=null;
+		
+		while(currentNode!=null) {
+			nextNode=currentNode.next();
+			currentNode.setNext(previousNode);
+			previousNode=currentNode;
+			currentNode=nextNode;
+		}
+		head=previousNode;
+			
+	}
+
+	/**
+	 * @return int
+	 */
+	public int size() {
+		int numberOfElements = 0;
+		if (head == null) {
+			throw new NullPointerException("Single Listed link not Initialized");
+		}
+		@SuppressWarnings("unchecked")
+		Node<T> tmp = head;
+		// traverse the list and count
+		while (true) {
+			if (tmp == null) {
+				break;
+			}
+			tmp = tmp.next();
+			numberOfElements++;
+		}
+		return numberOfElements;
+	}
+
+	/**
+	 * This method quickly allows to determine is the List is empty.
+	 * 
+	 * @return boolean
+	 */
+	public boolean isEmpty() {
+		if (head == null) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * this method is used to determine if a certain element is part of the list.
+	 * 
+	 * @param element
+	 * @return boolean
+	 */
+	public boolean contains(T element) {
+
+		if (head == null) {
+			return false; // list is empty...so it does not contains anything.
+		}
+		boolean opResult = false;
+		@SuppressWarnings("unchecked")
+		Node<T> tmp = head;
+		while (true) {
+			if (tmp == null) {
+				break;
+			}
+			if (tmp.getData() == element) {
+				opResult = true;
+				break;
+			}
+			tmp = tmp.next();
+		}
+		return opResult;
+	}
+	
+	//bellow are implementations of methods that override
+	//standard ones.
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || this.head == null)
+			return false;
+		SinglyLinkedList<T> sl = null;
+		
+		try {
+			sl = (SinglyLinkedList<T>) obj;
+		} catch (ClassCastException e) {
+			return false;
+		}
+		
+		boolean equality = true; // Initially we assume they are equal
+		Node<T> tmp1 = head;
+		Node<T> tmp2 = sl.head;
+		while (true) {
+			if (tmp1 == null || tmp2 == null) {// when null..all elements have been compared.
+				break;
+			}
+			// compare
+			T data1 = tmp1.data;
+			T data2 = tmp2.data;
+			if (!(data1.equals(data2))) {
+				equality = false;
+				break;
+			}
+			tmp1 = tmp1.next();
+			tmp2 = tmp2.next();
+		}
+		return equality;
+	}
+
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
+			@SuppressWarnings("unchecked")
+			Node<T> currentNode = head;
+
+			public boolean hasNext() {
+				boolean check;
+				try {
+					check = currentNode.hasNext();
+				} catch (NullPointerException e) {
+					check = false;
+				}
+				return check;
+			}
+
+			public T next() {
+				T data = null;
+				if (currentNode == head) {
+					data = currentNode.data;
+					currentNode = currentNode.next();
+					return data;
+				} else
+					data = currentNode.data;
+				currentNode = currentNode.next();
+				return data;
+			}
+		};
+	}
+
+	/**
+	 * A private inner class representing a node of the list.
+	 * 
+	 * @author martins
+	 *
+	 * @param <T>
+	 */
+	private static class Node<T> implements Comparable<T> {
+		private T data;
+		private Node<T> linksTo;
+
+		public T getData() {
+			return data;
+		}
+
+		public void setData(T data) {
+			this.data = data;
+		}
+
+		public Node<T> next() {
+			return linksTo;
+		}
+
+		public void setNext(Node<T> next) {
+			this.linksTo = next;
+		}
+
+		public boolean hasNext() {
+			return (this != null);
+		}
+
+		public int compareTo(T o) {
+			if (o == this.data) {
+				return 0;
+			} else {
+				return 1;
+			}
+		}
+	}
+
+}
---
+++ singlyLinkedList/src/test/java/com/noshio/singlyLinkedList/SinglyLinkedListTest.java
@@ -0,0 +1,134 @@
+package com.noshio.singlyLinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class SinglyLinkedListTest extends TestCase {
+	private SinglyLinkedList<Integer> sl1 = new SinglyLinkedList<Integer>();
+	private SinglyLinkedList<Integer> sl2 = new SinglyLinkedList<Integer>();
+
+	protected void setUp() throws Exception {
+		super.setUp();
+
+	}
+
+	@Test
+	public void testAdd() {
+		sl1.add(10);
+		sl1.add(20);
+		sl1.add(30);
+		sl2.add(10);
+		sl2.add(20);
+		sl2.add(30);
+		assertNotNull(sl1);
+		assertNotNull(sl2);
+		assertTrue(sl1.equals(sl2));
+
+	}
+
+	@Test
+	public void testAddIfAbsent() {
+		sl1.add(10);
+		sl1.add(20);
+		sl1.add(30);
+		assertNotNull(sl1);
+		assertTrue(sl1.addIfAbsent(50));
+	}
+
+	@Test
+	public void testAddAfter() {
+		sl1.add(10);
+		sl1.add(20);
+		sl1.add(30);
+		sl2.add(10);
+		sl2.add(30);
+		assertNotNull(sl1);
+		assertNotNull(sl2);
+		sl2.addAfter(20, 10);// adding element 20 after element 10;
+		assertTrue(sl1.equals(sl2));
+	}
+
+	@Test
+	public void testDeleteFront() {
+		sl1.add(10);
+		sl1.add(20);
+		sl1.add(30);
+
+		sl2.add(20);
+		sl2.add(30);
+		assertNotNull(sl1);
+		assertNotNull(sl2);
+		sl1.deleteFront();
+		assertTrue(sl1.equals(sl2));
+	}
+
+	@Test
+	public void testDeleteAfter() {
+
+		sl1.add(10);
+		sl1.add(20);
+		sl1.add(30);
+
+		sl2.add(10);
+
+		sl2.add(30);
+		assertNotNull(sl1);
+		assertNotNull(sl2);
+		sl1.deleteAfter(10);
+		assertTrue(sl1.equals(sl2));
+	}
+
+	@Test
+	public void testSize() {
+		sl1.add(10);
+		sl1.add(20);
+		sl1.add(30);
+		assertNotNull(sl1);
+
+		assertEquals(3, sl1.size());
+	}
+
+	@Test
+	public void testContains() {
+		sl1.add(10);
+		sl1.add(20);
+		sl1.add(30);
+		assertTrue(sl1.contains(10));
+		assertTrue(sl1.contains(20));
+		assertTrue(sl1.contains(30));
+	}
+
+	@Test
+	public void testIterator() {
+		sl1.add(10);
+		sl1.add(20);
+		sl1.add(30);
+		assertNotNull(sl1);
+		assertNotNull(sl2);
+		List<Integer> testvalues = new ArrayList<Integer>();
+		testvalues.add(10);
+		testvalues.add(20);
+		testvalues.add(30);
+		for (Integer elem : sl1) {
+			assertTrue(testvalues.contains(elem));
+		}
+	}
+	
+	@Test
+	public void testReverse() {
+		SinglyLinkedList<Integer> sl=new SinglyLinkedList<Integer>();
+		sl.add(10);
+		sl.add(20);
+		sl.add(30);
+		sl.reverse();
+		assertNotNull(sl1);
+	}
+	
+	
+
+
+}
